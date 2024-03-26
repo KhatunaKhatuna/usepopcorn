16dfcd747f7f8d37150f2607bd22d9a167f8ea40
KhatunaKhatuna <kh.chitishvili@gmail.com>
2024-03-27 02:37:01 +0400

    Make StarRating component more reusable

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./components/StarRating";
+
+function TestStarRating() {
+  const [movieRaiting, setMovieRaiting] = useState(0);
+  return (
+    <div>
+      <StarRating
+        maxRating={5}
+        color={"red"}
+        size={30}
+        messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+        defaultRating={3}
+        onSetRating={setMovieRaiting}
+      />
+      <p>This movies was rated {movieRaiting} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <TestStarRating />
   </React.StrictMode>
 );
